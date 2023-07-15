80ea6fc4df17083470e8b220ac60533962e4cee3
ron282 <ronan35@gmx.fr>
2023-07-15 18:48:31 +0200

    manage msec with datetime to string

--- src/base/QXmppUtils.cpp
+++ src/base/QXmppUtils.cpp
@@ -109,11 +109,13 @@ QDateTime QXmppUtils::datetimeFromString(const QString &str)
 QString QXmppUtils::datetimeToString(const QDateTime &dt)
 {
     // https://stackoverflow.com/questions/9527960/how-do-i-construct-an-iso-8601-datetime-in-c
-#if 0
     if (dt.time().msec()) {
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+        return dt.toUTC().toString(Qt::ISODate)+QString().sprintf(".%03d", dt.time().msec());
+#else
         return dt.toUTC().toString(Qt::ISODateWithMs);
-    }
 #endif
+    }
     return dt.toUTC().toString(Qt::ISODate);
 }
 
